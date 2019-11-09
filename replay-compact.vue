1dcbac10925e20fa0b84b08d1b1d13870c325948
lanyi <lanyi@ra3.moe>
2019-11-09 07:31:56 +0100

    show replay saver

--- replay-compact.vue
+++ replay-compact.vue
@@ -70,7 +70,8 @@
     <div class="replay-description inline-block" v-if="expanded">
         {{ replay.description }} <br/>
         地图名称：{{ replay.mapName }} <br/>
-        玩家列表：{{ replay.players.map(team => team.map(player => player.name).join(', ')).join(' vs ') }}
+        玩家列表：{{ replay.players.map(team => team.map(player => player.name).join(', ')).join(' vs ') }}<br/>
+        录像是{{ replaySaver }}保存的
     </div>
 </div>
 </template>
@@ -313,6 +314,11 @@ module.exports = {
         },
         downloadFileName() {
             return this.replay.fileName;
+        },
+        replaySaver() {
+            return this.replay.players
+            .map(team => team.filter(player => player.isSaver).map(player => player.name))
+            .filter(team => team.length > 0);
         }
     },
 }
