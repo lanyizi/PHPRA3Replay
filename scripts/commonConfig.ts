a2d8b60f570631b5f39ad97cb9343ed64232503b
lanyi <lanyi@ra3.moe>
2020-01-10 18:56:38 +0100

    fix typo

--- scripts/commonConfig.ts
+++ scripts/commonConfig.ts
@@ -1,6 +1,6 @@
 export const apiUrl = "/replays/replay.php";
 export const replaySettings = Object.freeze({
-    defaultMapImagePath: "/replays/ra3images/v1/factions/*.png",
+    defaultMapImagePath: "/replays/ra3images/v1/factions/7.png",
     mapImageFormat: "/replays/ra3images/v1/maps/*_art.png",
-    factionIconFormat: "/replays/ra3images/v1/factions/7.png",
+    factionIconFormat: "/replays/ra3images/v1/factions/*.png",
 })
